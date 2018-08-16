88f9fad438a02340a7dd393447cec2cdf4af9430
Julius Tens <mail@juliustens.eu>
2018-08-16 04:42:54 +0200

    add validateArguments.origin, validateArguments.destination; fix validateArguments.station

--- test.js
+++ test.js
@@ -10,29 +10,30 @@ const nonString = 12345
 const shortString = 'a'
 
 tape('fptf-util.validateArguments', (t) => {
-	// station
-	const validateStation = util.validateArguments.station
-	t.ok(isFunction(validateStation), 'station')
+	// station, origin, destination
+	for (let validateStation of [util.validateArguments.station, util.validateArguments.origin, util.validateArguments.destination]) {
+		t.ok(isFunction(validateStation), 'station')
 
-	t.ok(validateStation(urlSafe), 'station')
-	t.throws(() => validateStation(nonUrlSafe), 'station')
-	t.throws(() => validateStation(nonString), 'station')
+		t.ok(validateStation(urlSafe), 'station')
+		t.throws(() => validateStation(nonUrlSafe), 'station')
+		t.throws(() => validateStation(nonString), 'station')
 
-	t.ok(validateStation({type: 'station', id: urlSafe, name: urlSafe}), 'station')
-	t.throws(() => validateStation({type: 'station', id: nonUrlSafe, name: urlSafe}), 'station')
-	t.throws(() => validateStation({type: 'station', id: nonString, name: urlSafe}), 'station')
+		t.ok(validateStation({type: 'station', id: urlSafe, name: urlSafe}), 'station')
+		t.throws(() => validateStation({type: 'station', id: nonUrlSafe, name: urlSafe}), 'station')
+		t.throws(() => validateStation({type: 'station', id: nonString, name: urlSafe}), 'station')
 
-	t.throws(() => validateStation(''), 'station')
-	t.throws(() => validateStation({type: 'station', id: '', name: urlSafe}), 'station')
-	t.throws(() => validateStation(), 'station')
-	t.throws(() => validateStation({type: 'station', name: urlSafe}), 'station')
-	t.throws(() => validateStation(null), 'station')
-	t.throws(() => validateStation({type: 'station', id: null, name: urlSafe}), 'station')
+		t.throws(() => validateStation(''), 'station')
+		t.throws(() => validateStation({type: 'station', id: '', name: urlSafe}), 'station')
+		t.throws(() => validateStation(), 'station')
+		t.throws(() => validateStation({type: 'station', name: urlSafe}), 'station')
+		t.throws(() => validateStation(null), 'station')
+		t.throws(() => validateStation({type: 'station', id: null, name: urlSafe}), 'station')
 
-	t.throws(() => validateStation({type: 'region', id: urlSafe, name: urlSafe}), 'station')
-	t.throws(() => validateStation({type: 'stop', id: urlSafe, name: urlSafe}), 'station')
-	t.throws(() => validateStation({id: urlSafe, name: urlSafe}), 'station')
-	t.throws(() => validateStation({type: 'station', id: urlSafe}), 'station')
+		t.throws(() => validateStation({type: 'region', id: urlSafe, name: urlSafe}), 'station')
+		t.throws(() => validateStation({type: 'stop', id: urlSafe, name: urlSafe}), 'station')
+		t.throws(() => validateStation({id: urlSafe, name: urlSafe}), 'station')
+		t.throws(() => validateStation({type: 'station', id: urlSafe}), 'station')
+	}
 
 
 	// query
